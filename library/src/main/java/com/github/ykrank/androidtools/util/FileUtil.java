dea308d18e9ba1e8edb5f20569c4dcb4a902d340
UFR6cRY9xufLKtx2idrc <68910835+UFR6cRY9xufLKtx2idrc@users.noreply.github.com>
2024-03-11 14:50:10 +0000

    Update FileUtil.java

--- library/src/main/java/com/github/ykrank/androidtools/util/FileUtil.java
+++ library/src/main/java/com/github/ykrank/androidtools/util/FileUtil.java
@@ -121,7 +121,11 @@ public class FileUtil {
                 return ".raw";
             case WEBP:
             case WEBP_A:
+            case ANIMATED_WEBP:
                 return ".webp";
+            case AVIF:
+            case ANIMATED_AVIF:
+                return ".avif";
             default:
                 return null;
         }
